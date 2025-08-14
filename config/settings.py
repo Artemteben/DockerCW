3dfc534420f11d6fedd136c654e97ddfad0ad548
Artemteben <artyom.teben@bk.ru>
2025-08-14 21:14:50 +0300

    pochti_final1

--- config/settings.py
+++ config/settings.py
@@ -10,7 +10,7 @@ load_dotenv(BASE_DIR / ".env")
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-DEBUG = config("DEBUG", False) == "True"
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -116,6 +116,10 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "static/"
+STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 NULLABLE = {"null": True, "blank": True}
