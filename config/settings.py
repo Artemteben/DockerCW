6ee1ac6816dda3e58480fb666cd420b2fa2522bd
Artemteben <artyom.teben@bk.ru>
2025-08-15 18:52:36 +0300

    risk_debugfffssss

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR / ".env")
 
 SECRET_KEY = os.getenv("SECRET_KEY")
-DEBUG = "True"
+DEBUG = True
 
 ALLOWED_HOSTS = ['51.250.33.202', 'localhost']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
